66f5870d5ef6ece88c42dd1e711caed3c97bb4e4
Shivam Amin <11432922+ShivamAmin@users.noreply.github.com>
2025-03-19 14:33:57 -0600

    move loading playlists to page component

--- app/(root)/playlists/page.tsx
+++ app/(root)/playlists/page.tsx
@@ -3,12 +3,25 @@ import PlaylistList from '@/components/playlists/playlistList'
 import Header from "@/components/page/header";
 import Content from "@/components/page/content";
 import Main from "@/components/page/main";
+import { cookies } from "next/headers";
 
 async function Page() {
+    const url = "https://plex.shivamamin.com/playlists?playlistType=audio";
+    const cookieStore = await cookies();
+    const plexAuthToken = cookieStore.get('plex_auth_token');
+    const req = await fetch(url, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            'X-Plex-Token': plexAuthToken!.value
+        }
+    });
+    const resp = await req.json();
+    const playlists = resp.MediaContainer.Metadata;
     return (
         <Main>
             <Header>Playlists</Header>
-            <Content><PlaylistList /></Content>
+            <Content><PlaylistList playlists={playlists} /></Content>
         </Main>
     )
 }
--- components/playlists/playlistList.tsx
+++ components/playlists/playlistList.tsx
@@ -1,41 +1,10 @@
-"use client"
-
-import {useEffect, useState} from "react";
-import usePlexOAuth from "@/hooks/usePlexOAuth";
 import PlaylistItem from "./playlistItem";
+import playlist from '@/types/playlist'
 
-function PlaylistList() {
-    const [loading, setLoading] = useState(true);
-    const [playlists, setPlaylists] = useState([]);
-    const { plexAuthToken } = usePlexOAuth();
-    useEffect(() => {
-        const controller = new AbortController();
-        const getPlaylists = async () => {
-            const url = "https://plex.shivamamin.com/playlists?playlistType=audio"
-            try {
-                return await fetch(url, {
-                    signal: controller.signal,
-                    method: "GET",
-                    headers: {
-                        Accept: "application/json",
-                        'X-Plex-Token': plexAuthToken!
-                    }
-                })
-                    .then((res) => res.json())
-                    .then((data) => setPlaylists(data.MediaContainer.Metadata))
-                    .then(() => setLoading(prev => !prev));
-            } catch (e) {
-            }
-        }
-        getPlaylists();
-        return () => {
-            controller.abort('Request cancelled unexpectedly');
-        };
-    }, []);
-
+function PlaylistList({ playlists }: { playlists: playlist[] }) {
     return (
-        <div className={`grid justify-center gap-4 grid-cols-[repeat(auto-fit,minmax(280px,max-content))]`} /*className={'flex flex-wrap gap-4 justify-center'}*/>
-            {!loading && playlists.map((playlist: any) => (
+        <div className={`grid justify-center gap-4 grid-cols-[repeat(auto-fit,minmax(280px,max-content))]`}>
+            {playlists.map((playlist: playlist) => (
                 <PlaylistItem playlist={playlist} key={playlist.key} />
             ))}
         </div>
