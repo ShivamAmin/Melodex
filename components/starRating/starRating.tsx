5b80bb80a90067f688392f5320cc56821c4058b0
Shivam Amin <11432922+ShivamAmin@users.noreply.github.com>
2025-03-27 13:27:43 -0600

    implement rateTrack server action

--- components/starRating/starRating.tsx
+++ components/starRating/starRating.tsx
@@ -3,19 +3,17 @@
 import starRating from '@/types/starRating'
 import { useState } from "react";
 import Star from '@/components/starRating/star';
+import rateTrack from '@/actions/rateTrack';
 
-function StarRating({
-    max = 5,
-    starColour = "#fcc149",
-    size = 18,
-    initialRating = 0,
-    onSetRating = (rating: number) => {
-        return rating;
-    },
-    disabled = false,
-}: starRating) {
+function StarRating({ librarySectionID, ratingKey, max = 5, starColour = "#fcc149", initialRating = 0, disabled = false }: starRating) {
     const [rating, setRating] = useState(initialRating);
     const [hoverRating, setHoverRating] = useState(0);
+
+    const handleRating = (userRating: number) => {
+        setRating(userRating);
+        rateTrack(librarySectionID, ratingKey, userRating);
+    }
+
     const getFill = (idx: number): 'empty' | 'half' | 'full' => {
         if (!!hoverRating) {
             if (hoverRating >= idx) {
@@ -35,23 +33,16 @@ function StarRating({
             }
         }
     }
+
     return (
         <div className={'flex'}>
             {Array.from({ length: max }, (_, i) => (
                 <Star
                     starColour={starColour}
-                    size={size}
                     key={i}
                     onRate={(side: string) => {
-                        if (!disabled) {
-                            if (side === 'left') {
-                                setRating(((i + 1) * 2) - 1)
-                                onSetRating(((i + 1) * 2) - 1)
-                            } else {
-                                setRating((i + 1) * 2)
-                                onSetRating((i + 1) * 2)
-                            }
-                        }
+                        const rating = side === 'left' ? ((i + 1) * 2) - 1 : (i + 1) * 2;
+                        handleRating(rating);
                     }}
                     fill={getFill((i + 1) * 2)}
                     onHover={(side: string) => {
