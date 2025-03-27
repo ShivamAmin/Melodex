6ace44bba7eac1e5eeb4a13c5c46a440c16c96bd
Shivam Amin <11432922+ShivamAmin@users.noreply.github.com>
2025-03-27 08:11:33 -0600

    Add loading skeleton to plexImage Update plexImage to load image properly

--- components/plexImage.tsx
+++ components/plexImage.tsx
@@ -1,5 +1,7 @@
 "use client"
+
 import Image, { ImageProps } from "next/image";
+import { Skeleton } from '@/components/ui/skeleton';
 import usePlexOAuth from "@/hooks/usePlexOAuth";
 import { useState, useEffect } from "react";
 
@@ -11,22 +13,43 @@ function PlexImage(props: ImageProps) {
     const [imageSRCWithAuth, setImageSRCWithAuth] = useState<string>('');
 
     useEffect(() => {
+        const controller = new AbortController();
         const plexImageUrl = 'https://plex.shivamamin.com' + src;
-        if (plexAuthToken) {
-            fetch(plexImageUrl, {
-                method: 'GET',
-                headers: {
-                    'X-Plex-Token': plexAuthToken
+
+        const getImage = async () => {
+            if (plexAuthToken) {
+                try {
+                    await fetch(plexImageUrl, {
+                        method: 'GET',
+                        headers: {
+                            'X-Plex-Token': plexAuthToken
+                        }
+                    })
+                        .then(res => res.blob())
+                        .then(blob => setImageSRCWithAuth(URL.createObjectURL(blob)))
+                        .then(() => {
+                            setLoading(false);
+                        });
+                } catch (e) {
+                    setLoading(true);
                 }
-            })
-                .then(res => res.blob())
-                .then(blob => setImageSRCWithAuth(URL.createObjectURL(blob)))
-                .then(() => setLoading(false));
+            }
         }
+
+        getImage();
+
+        return () => {
+            controller.abort('Request cancelled unexpectedly');
+        };
+
     }, [plexAuthToken]);
 
     if (loading) {
-        return <>Loading...</>
+        if (restProps?.fill) {
+            return <Skeleton className={'absolute w-full h-full inset-0'} />;
+        } else {
+            return <Skeleton className={'w-full h-full'} />;
+        }
     }
 
     return (
