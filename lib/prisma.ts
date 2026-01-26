ed819fae22c61d969a71430c6076e383e8e88ac8
Shivam Amin <11432922+ShivamAmin@users.noreply.github.com>
2026-01-25 19:55:52 -0700

    Upgrade prisma to latest

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from './generated/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
 
 const prismaClientSingleton = () => {
-    return new PrismaClient();
+    return new PrismaClient({ adapter });
 };
 
 declare const globalThis: {
